4d76b48c4e15a80cb8c2f58c85b017dd5f1b5d0b
Youxuf <73359716+Youxuf@users.noreply.github.com>
2024-11-26 11:49:43 +0000

    task 5-get-articles

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -79,7 +79,7 @@ describe("GET /api/articles/:article_id", () => {
   });
 });
 
-describe.only("GET /api/articles", () => {
+describe("GET /api/articles", () => {
   test("200: returns all the articles sorted by date in descending order", () => {
     return request(app)
       .get("/api/articles")
